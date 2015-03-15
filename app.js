da6d1a6c54cd17cb80d76f09046a70a3101f0832
Yin Zhao <kidzhaoyin@gmail.com>
2015-03-15 13:40:43 -0400

    fixed path errors

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ var morgan = require('morgan');
 var swig = require('swig');
 
 app.use(morgan('dev'));
-app.set('view engine','html');
 
 app.engine('html',swig.renderFile);
-app.set('views',__dirname + './views');
+app.set('views',__dirname + '/views/');
 swig.setDefaults({cache: false});
-
+app.set('view engine','html');
 app.get('/', function (request, response){
-	response.send('hello');
-
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+response.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 var server = app.listen(3000, function(){
