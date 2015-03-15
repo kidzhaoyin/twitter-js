12278b9d6e7fe22df094213087272058969ebd75
Yin Zhao <kidzhaoyin@gmail.com>
2015-03-15 12:49:57 -0400

    added settings

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require ('express');
 var app = express();
-var morgan = require('morgan')
+var morgan = require('morgan');
+var swig = require('swig');
+
 app.use(morgan('dev'));
+app.set('view engine','html');
+
+app.engine('html',swig.renderFile);
+app.set('views',__dirname + './views');
+swig.setDefaults({cache: false});
 
 app.get('/', function (request, response){
 	response.send('hello');
