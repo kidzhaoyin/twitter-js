ccdecee66133a22d08b4e7bfa9bf889ae5042677
Yin Zhao <kidzhaoyin@gmail.com>
2015-03-15 23:46:29 -0400

    changes explained in the previous commit; finished

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
+module.exports = function(io){
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
   res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
@@ -11,7 +12,24 @@ router.get('/', function (req, res) {
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   var list = tweetBank.find( {name: name} );
-  res.render( 'index', { title: 'Twitter.js - Posts by '+name, list: list, showForm:true, name:name } );
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list, showForm:true } );
 });
 
-module.exports = router;
+router.get('/users/:name/tweets/:id', function(req, res) {
+  var id = req.params.id;
+  var list = tweetBank.find( {id: id} );
+  res.render( 'index', { title: 'Tiwitter.js - Posts by ' + id, tweets: list });
+});
+
+
+router.post('/submit', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+
+  io.sockets.emit('new_tweet', {name: name, text: text});
+});
+return router;
+}
+//module.exports = router;
