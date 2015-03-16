470e4a4c6b1389f6244908b24fd90e4a3af6b144
Yin Zhao <kidzhaoyin@gmail.com>
2015-03-15 21:41:05 -0400

    finally got the browser work by fixing a deleted listening statement

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+
+
 var express = require ('express');
 var app = express();
+var router = express.Router();
 var morgan = require('morgan');
 var swig = require('swig');
+var routes = require('./routes/');
 
+app.use('/', routes);
 app.use(morgan('dev'));
 
+app.use(express.static(__dirname + '/public'));
 app.engine('html',swig.renderFile);
 app.set('views',__dirname + '/views/');
 swig.setDefaults({cache: false});
 app.set('view engine','html');
-app.get('/', function (request, response){
-var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-response.render( 'index', {title: 'Hall of Fame', people: people} );
-});
+//app.get('/', function (request, response){
+//var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//response.render( 'index', {title: 'Hall of Fame', people: people} );
+//});
 
 var server = app.listen(3000, function(){
-	console.log("server listening");
+	console.log('welcome');
+
 });
