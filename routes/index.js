fb803659e63b9b8005a295f74f62118f55e31c6a
Yin Zhao <kidzhaoyin@gmail.com>
2015-03-15 23:02:49 -0400

    routing with parameters; add a single-user route and view

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,11 @@ router.get('/', function (req, res) {
   var tweets = tweetBank.list();
   res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
 });
+//var tweets = tweetBank.list();
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name, list: list, showForm:true, name:name } );
+});
 
 module.exports = router;
